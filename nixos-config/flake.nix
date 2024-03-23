358e0b31750d749b7bc0a643869dd2f674de748f
Scaniox <alexl.mills3@gmail.com>
2024-03-23 00:19:30 +0000

    initial commit

---
+++ nixos-config/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    minegrub-theme.url = "github:Lxtharia/minegrub-theme";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/expertbook/configuration.nix
+        inputs.home-manager.nixosModules.default
+        inputs.minegrub-theme.nixosModules.default
+      ];
+    };
+  };
+}

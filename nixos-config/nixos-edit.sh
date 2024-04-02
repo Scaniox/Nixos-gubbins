6b54d092bb4ed2d1f9966bf6b6564a5df304d1e3
scaniox <alexl.mills3@gmail.com>
2024-04-02 22:20:54 +0100

    fix generation acquisition

--- nixos-config/nixos-edit.sh
+++ nixos-config/nixos-edit.sh
@@ -41,7 +41,7 @@ echo "NixOS Rebuilding..."
 sudo nixos-rebuild switch --flake .# # &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
 
 # Get current generation metadata
-current=$(nixos-rebuild list-generations | grep current)
+current=$(nixos-rebuild --flake .# list-generations | grep current)
 
 # Commit all changes witih the generation metadata
 git commit -am "$current"

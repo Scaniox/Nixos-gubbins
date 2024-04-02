9ed8b097faf51e413060ee316b3a67fa6c81738e
scaniox <alexl.mills3@gmail.com>
2024-04-02 23:51:34 +0100

    @DESKTOP-M60QOUU  13 current  2024-04-02 23:32:48  24.05.20240321.20f77aa   6.6.22                          *

--- nixos-config/nixos-edit.sh
+++ nixos-config/nixos-edit.sh
@@ -23,11 +23,6 @@ git pull
 # $EDITOR ~/nixos-gubbins/nixos-config/hosts/home.nix
 sudo code ~/nixos-gubbins/nixos-config/nixos-config.code-workspace --wait --no-sandbox --user-data-dir
 
-if [[[[ "$@" =~ "rebuild" ]] and ! [[ "$@" =~ "rebuild" ]]]]; then
-   echo "rebuilding"
-fi
-
-
 # Early return if no changes were detected (thanks @singiamtel!)
 if [[ "$@" =~ "rebuild" ]] && git diff --quiet '*.nix'; then
     echo "No changes detected, exiting."

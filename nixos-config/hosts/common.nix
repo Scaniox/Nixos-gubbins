71565a82fc31dd09a32aa0386ca6f9fcba594aff
scaniox <alexl.mills3@gmail.com>
2024-05-20 17:43:54 +0100

    @DESKTOP-M60QOUU  85 current  2024-05-20 17:43:45  24.05.20240511.062ca2a   6.6.30                          *

--- nixos-config/hosts/common.nix
+++ nixos-config/hosts/common.nix
@@ -186,5 +186,5 @@
   networking.firewall.allowedTCPPorts = [5201];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 }

0d632baba4b6a5a7ae932438a7b97c0368a88b79
scaniox <alexl.mills3@gmail.com>
2024-05-20 17:49:17 +0100

    @DESKTOP-M60QOUU  86 current  2024-05-20 17:49:13  24.05.20240511.062ca2a   6.6.30                          *

--- nixos-config/hosts/common.nix
+++ nixos-config/hosts/common.nix
@@ -183,7 +183,7 @@
   hardware.logitech.wireless.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [5201];
+  networking.firewall.allowedTCPPorts = [5201 24800];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
